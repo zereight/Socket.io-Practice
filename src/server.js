d457f2c38da63b7cb7755648eacdef0e5fc1a6c7
zereight <duaxka7@naver.com>
2019-11-10 09:35:26 +0900

    Eslint Setu

--- src/server.js
+++ src/server.js
@@ -2,15 +2,18 @@ import express from "express";
 import path from "path";
 import SocketIO from "socket.io";
 
-const PORT = 4000
+const PORT = 4000;
 const app = express();
-
 
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 
-app.use( express.static(path.join(__dirname, "static") ));
+app.use(express.static(path.join(__dirname, "static")));
 
-app.get("/", (req,res)=> {res.render("home");})
+app.get("/", (req, res) => {
+  res.render("home");
+});
 
-app.listen(PORT, ()=> {console.log("✅ Listening!!")})
+app.listen(PORT, () => {
+  console.log("✅ Listening!!");
+});
