397e7638ebcdbbd9baa3dd443770bee7d4a83fac
zereight <duaxka7@naver.com>
2019-11-10 09:26:09 +0900

    Server Setup
    static for front-end

--- src/server.js
+++ src/server.js
@@ -1,2 +1,16 @@
 import express from "express";
+import path from "path";
+import SocketIO from "socket.io";
 
+const PORT = 4000
+const app = express();
+
+
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+
+app.use( express.static(path.join(__dirname, "static") ));
+
+app.get("/", (req,res)=> {res.render("home");})
+
+app.listen(PORT, ()=> {console.log("✅ Listening!!")})
